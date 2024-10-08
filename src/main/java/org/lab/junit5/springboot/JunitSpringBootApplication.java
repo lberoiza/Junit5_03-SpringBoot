52614d5079f3b93d7ef1b45424e002f0dbe39f2a
lberoiza <luis.beroiza@adesso.de>
2024-10-08 20:59:26 +0200

    refactor: change name of Application Starter

--- src/main/java/org/lab/junit5/springboot/JunitSpringBootApplication.java
+++ src/main/java/org/lab/junit5/springboot/JunitSpringBootApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Junit503JunitSpringBootApplication {
+public class JunitSpringBootApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(Junit503JunitSpringBootApplication.class, args);
+    SpringApplication.run(JunitSpringBootApplication.class, args);
   }
 
 }
