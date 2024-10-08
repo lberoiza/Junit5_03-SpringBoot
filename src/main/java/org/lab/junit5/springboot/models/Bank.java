2e2d0a962eab7daa97e7a25744640183412ad4f8
lberoiza <luis.beroiza@adesso.de>
2024-10-08 22:03:20 +0200

    fix: remove constructor with all arguments

--- src/main/java/org/lab/junit5/springboot/models/Bank.java
+++ src/main/java/org/lab/junit5/springboot/models/Bank.java
@@ -1,12 +1,10 @@
 package org.lab.junit5.springboot.models;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
 @Data
 @Accessors(chain = true)
-@AllArgsConstructor
 public class Bank {
   private Long id;
   private String name;
