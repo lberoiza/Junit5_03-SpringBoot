63c93ed2381d099e293e6023f0de4093422fc006
lberoiza <luis.beroiza@adesso.de>
2024-10-08 22:03:39 +0200

    feat: add TestDataBuilders

---
+++ src/test/java/org/lab/junit5/springboot/testdata/AccountTestDataBuilder.java
@@ -0,0 +1,35 @@
+package org.lab.junit5.springboot.testdata;
+
+import lombok.AllArgsConstructor;
+import lombok.With;
+import net.datafaker.Faker;
+import org.lab.junit5.springboot.models.Account;
+
+import java.math.BigDecimal;
+import java.util.Locale;
+
+@With
+@AllArgsConstructor
+public class AccountTestDataBuilder {
+
+  private static final Faker faker = new Faker(Locale.of("es"));
+
+  private Long id = faker.number().randomNumber();
+  private String accountNumber = faker.finance().iban();
+  private String owner = faker.name().fullName();
+  private BigDecimal balance = BigDecimal.valueOf(faker.number().randomDouble(2, 0, 1000));
+
+  private AccountTestDataBuilder() {}
+
+  private AccountTestDataBuilder random() {
+    return new AccountTestDataBuilder();
+  }
+
+  public Account build() {
+    return new Account()
+        .setId(id)
+        .setAccountNumber(accountNumber)
+        .setOwner(owner)
+        .setBalance(balance);
+  }
+}
---
+++ src/test/java/org/lab/junit5/springboot/testdata/BankTestDataBuilder.java
@@ -0,0 +1,32 @@
+package org.lab.junit5.springboot.testdata;
+
+import java.util.Locale;
+import lombok.AllArgsConstructor;
+import lombok.With;
+import net.datafaker.Faker;
+import org.lab.junit5.springboot.models.Bank;
+
+@With
+@AllArgsConstructor
+public class BankTestDataBuilder {
+
+  private static final Faker faker = new Faker(Locale.of("es"));
+
+  private Long id = faker.number().randomNumber();
+  private String name = "Bank: %s".formatted(faker.finance().creditCard());
+  private String owner = faker.name().fullName();
+  private int totalOfTransfers =faker.number().numberBetween(0, 100);
+
+  private BankTestDataBuilder() {}
+
+  private BankTestDataBuilder random() {
+    return new BankTestDataBuilder();
+  }
+
+  public Bank build() {
+    return new Bank()
+        .setId(id)
+        .setName(name)
+        .setTotalOfTransfers(totalOfTransfers);
+  }
+}
