48d559a670ebe7a9ad52f2010ef06c668356b619
lberoiza <luis.beroiza@adesso.de>
2024-10-08 20:22:33 +0200

    init: Initial commit of Project to play with JUnit, Mockito in Spring Boot

---
+++ src/main/java/org/lab/junit5/springboot/Junit503JunitSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.lab.junit5.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit503JunitSpringBootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Junit503JunitSpringBootApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/lab/junit5/springboot/Junit503JunitSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lab.junit5.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit503JunitSpringBootApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
