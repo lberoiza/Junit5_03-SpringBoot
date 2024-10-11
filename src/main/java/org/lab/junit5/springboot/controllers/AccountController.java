2274e1c3b00063a280be6e7d635a78746775b720
lberoiza <luis.beroiza@adesso.de>
2024-10-12 01:31:46 +0200

    fix: Controller Url

--- src/main/java/org/lab/junit5/springboot/controllers/AccountController.java
+++ src/main/java/org/lab/junit5/springboot/controllers/AccountController.java
@@ -14,7 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("/api/accounts")
+@RestController
+@RequestMapping("/api/accounts")
 public class AccountController {
 
   private final AccountService accountService;
