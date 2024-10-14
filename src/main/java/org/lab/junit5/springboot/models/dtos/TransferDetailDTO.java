5a49fab64f295b2647c00c8633a2bbd60d87f8cf
lberoiza <luis.beroiza@adesso.de>
2024-10-15 00:13:38 +0200

    feat: add validation method

--- src/main/java/org/lab/junit5/springboot/models/dtos/TransferDetailDTO.java
+++ src/main/java/org/lab/junit5/springboot/models/dtos/TransferDetailDTO.java
@@ -3,4 +3,9 @@ package org.lab.junit5.springboot.models.dtos;
 import java.math.BigDecimal;
 
 public record TransferDetailDTO(
-    Long sourceAccountId, Long targetAccountId, Long bankId, BigDecimal amount) {}
+    Long sourceAccountId, Long targetAccountId, Long bankId, BigDecimal amount) {
+
+  public boolean isNotValid() {
+    return sourceAccountId == null || targetAccountId == null || bankId == null || amount == null;
+  }
+}
