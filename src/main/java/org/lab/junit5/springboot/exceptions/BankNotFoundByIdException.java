76c38d5e80d71cd982a4a3f1c434c4ff74968fc7
lberoiza <luis.beroiza@adesso.de>
2024-10-15 09:36:40 +0200

    fix: BankNotFoundByIdException extends of BankException

--- src/main/java/org/lab/junit5/springboot/exceptions/BankNotFoundByIdException.java
+++ src/main/java/org/lab/junit5/springboot/exceptions/BankNotFoundByIdException.java
@@ -1,12 +1,10 @@
 package org.lab.junit5.springboot.exceptions;
 
-
-public class BankNotFoundByIdException extends AccountException{
+public class BankNotFoundByIdException extends BankException {
 
   private static final String message = "Bank with id '%s' not found.";
 
   public BankNotFoundByIdException(Long bankId) {
     super(message.formatted(bankId));
   }
-
 }
