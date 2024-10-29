6e0364cf39406a7d26a391cbc7dcff160e9bfb2e
lberoiza <luis.beroiza@adesso.de>
2024-10-29 09:27:00 +0100

    feat: add Test Profile to Start the Application and load Test Data to be tested as API-Rest

---
+++ src/main/java/org/lab/junit5/springboot/config/ProfileTestConfiguration.java
@@ -0,0 +1,54 @@
+package org.lab.junit5.springboot.config;
+
+import jakarta.annotation.PostConstruct;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.stream.Collectors;
+import javax.sql.DataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourcePatternResolver;
+
+@Profile("test")
+@Configuration
+@Slf4j
+public class ProfileTestConfiguration {
+
+  @Autowired private DataSource dataSource;
+
+  @Autowired private ResourcePatternResolver resourcePatternResolver;
+
+  @PostConstruct
+  public void loadTestData() {
+    try (Connection connection = dataSource.getConnection();
+        Statement statement = connection.createStatement()) {
+
+      // Cargar todos los archivos .sql en el directorio "testdata"
+      Resource[] resources = resourcePatternResolver.getResources("classpath:testdata/*.sql");
+
+      for (Resource resource : resources) {
+        try (BufferedReader reader =
+            new BufferedReader(new InputStreamReader(resource.getInputStream()))) {
+          // Leer y ejecutar el contenido de cada archivo SQL
+          String sqlScript = reader.lines().collect(Collectors.joining("\n"));
+          statement.execute(sqlScript);
+
+          log.info("Ejecutado archivo SQL: {}", resource.getFilename());
+        } catch (Exception e) {
+          log.error("Error al ejecutar '{}': {}", resource.getFilename(), e.getMessage());
+        }
+      }
+
+      log.info("Todos los archivos SQL en 'testdata' fueron ejecutados exitosamente.");
+
+    } catch (Exception e) {
+      log.error(
+          "Error al cargar datos de prueba desde los archivos en 'testdata': {}", e.getMessage());
+    }
+  }
+}
---
+++ src/main/resources/testdata/data-test.sql
@@ -0,0 +1,8 @@
+-- Agrega Banco de Prueba
+INSERT INTO banks (id, name, total_of_transactions) VALUES (1, 'Banco de Prueba', 0);
+
+-- Agrega Cuentas de Prueba
+INSERT INTO accounts (id, account_number, owner, balance) VALUES (1, '123456', 'Juan Perez', 1000);
+INSERT INTO accounts (id, account_number, owner, balance) VALUES (2, '654321', 'Maria Lopez', 2000);
+
+
